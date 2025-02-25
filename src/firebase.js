f679d4194de30574a58cb8d7d82025220e371d80
808drinks <85304504+808drinks@users.noreply.github.com>
2025-02-25 16:14:07 +0900

    Fix Netlify ESLint build error

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,20 @@
 // firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";  // 주석 처리
 
-// 환경 변수에서 Firebase 설정 값 불러오기
+// Firebase 설정
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Firebase 초기화
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
